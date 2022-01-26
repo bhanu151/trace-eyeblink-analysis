51b96e10186cf3f857825f927c075028266f6a7e
Bhanu <bhanupriya151@gmail.com>
2022-01-26 18:47:36 +0530

    decrement trial num for old animal as well

--- miscellaneous-scripts/decrement_file_num.sh
+++ miscellaneous-scripts/decrement_file_num.sh
@@ -1,90 +1,92 @@
 #!/bin/bash
 
-while getopts s:d flag
+while getopts s:a: flag
 do
-    case "${flag}" in
-        s) source=${OPTARG};;
-    esac
+  case "${flag}" in
+    s) source=${OPTARG};;
+    a) animal_id=${OPTARG};;
+  esac
 done
 echo "Source: $source";
+echo "Animal ID: $animal_id";
 cd $source
 pwd
 
-animal_list=$(find $source -type d -name G71)
+animal_list=$(find $source -type d -name $animal_id)
 for animal in $animal_list
 do
-    cd $animal
+  cd $animal
+  pwd
+  day_list=$(find $animal -type d -name 20\*)
+  for day in $day_list
+  do
+    cd $day
     pwd
-    day_list=$(find $animal -type d -name 20\*)
-    for day in $day_list
+    session_num_list=$(find $day -type d -name \[0-9])
+    for session in $session_num_list
     do
-        cd $day
-        pwd
-        session_num_list=$(find $day -type d -name \[0-9])
-        for session in $session_num_list
-        do
-            cd $session
-            pwd
-            if [ -f *Trial-01-ROI*.ti* ]; then
-                if [ -f *Trial-01-ROI*.xls ]; then
-                    continue 
-                fi
-            fi
+      cd $session
+      pwd
+      if [ -f *Trial-01-ROI*.ti* ]; then
+        # if [ -f *Trial-01-ROI*.xls ]; then
+        continue 
+        # fi
+      fi
 
-            file_list=$(find $session -type f -name \*Trial-\[0-9\]\*)
-            for file in $file_list
-            do
-                #readarray -d - -t arr <<< "$file" 
+      file_list=$(find $session -type f -name \*Trial-\[0-9\]\*)
+      for file in $file_list
+      do
+        #readarray -d - -t arr <<< "$file"
 
-                #echo $file
+        #echo $file
 
-                delimiter="-"
-                concat_str="$file"$delimiter 
+        delimiter="-"
+        concat_str="$file"$delimiter
 
-                arr=()
-                while [[ $concat_str ]]; do
-                    arr+=( "${concat_str%%"$delimiter"*}" )
-                    concat_str=${concat_str#*"$delimiter"}
-                done
-
-
-                #for (( n=0; n < ${#arr[*]}; n++))
-                #do
-                    #echo "${arr[n]}"
-                #done
-                temp_str="${arr[1]}"
-
-                old_file_num="${temp_str#"${temp_str%%[!0]*}"}"
-                new_file_num=$(expr $old_file_num - 1)
-
-                arr_len=${#arr[@]}
-
-                new_file_name=${arr[0]}"_renamed-"
-
-
-                if [ $new_file_num -lt 10 ] 
-                then
-                    new_file_name+="0"$new_file_num
-                else
-                    new_file_name+="$new_file_num"
-                fi
-
-
-                for (( i=2; i<$arr_len; i++ ))
-                do
-                    new_file_name+="-"${arr[$i]}
-                done
-                echo $old_file_num $new_file_num 
-
-                if [ -f $new_file_name ]
-                then
-                    echo "ERROR : $new_file_name exists"
-                else
-                    mv $file $new_file_name
-                fi
-            done
-            mmv \*_Trial_renamed-\* \#1_renamed_Trial-\#2
+        arr=()
+        while [[ $concat_str ]]; do
+          arr+=( "${concat_str%%"$delimiter"*}" )
+          concat_str=${concat_str#*"$delimiter"}
         done
+
+
+        #for (( n=0; n < ${#arr[*]}; n++))
+        #do
+          #echo "${arr[n]}"
+        #done
+        temp_str="${arr[1]}"
+
+        old_file_num="${temp_str#"${temp_str%%[!0]*}"}"
+        new_file_num=$(expr $old_file_num - 1)
+
+        arr_len=${#arr[@]}
+
+        new_file_name=${arr[0]}"_renamed-"
+
+
+        if [ $new_file_num -lt 10 ]
+        then
+          new_file_name+="0"$new_file_num
+      else
+          new_file_name+="$new_file_num"
+        fi
+
+
+        for (( i=2; i<$arr_len; i++ ))
+        do
+          new_file_name+="-"${arr[$i]}
+        done
+        echo $old_file_num $new_file_num
+
+        if [ -f $new_file_name ]
+        then
+          echo "ERROR : $new_file_name exists"
+        else
+          mv $file $new_file_name
+        fi
+      done
+      mmv \*_Trial_renamed-\* \#1_renamed_Trial-\#2
     done
+  done
 done
 
