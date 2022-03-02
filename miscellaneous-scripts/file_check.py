838f039d567a4b9e620d9e976aed43ec48de6b45
Bhanu Priya <bhanupriya151@gmail.com>
2022-03-02 18:46:27 +0530

    Checks for error files based on file size

--- miscellaneous-scripts/file_check.py
+++ miscellaneous-scripts/file_check.py
@@ -8,189 +8,21 @@ import os
 import pandas as pd
 
 
-def csv_with_imaging(csv_path, imaging_path, animals):
-    print("##################################################")
-    print("CSV WITH IMAGING")
-    print("##################################################")
-
-    for animal_name in animals:
-        csv_data = pd.read_csv(
-            csv_path + "/" + animal_name + ".csv",
-            dtype={
-                "upi": np.int64,
-                "date": str,
-                "experiment_number": str,
-                "num_imaging_trials": np.int64,
-            },
-        )
-
-        print("--------------------------------------------------")
-        print(animal_name)
-        print("--------------------------------------------------")
-
-        img_sess_paths = glob.glob(imaging_path + "/" + animal_name + "/20*/[0-9]")
-        img_dates = [x.split("/")[-2] for x in img_sess_paths]
-        for index, session in csv_data.iterrows():
-
-            print("**************************************************")
-            print(session["date"] + "/" + session["experiment_number"])
-            print("**************************************************")
-
-            if session["date"] in img_dates:
-                img_expt_nums = [
-                    x.split("/")[-1]
-                    for x in glob.glob(
-                        imaging_path
-                        + "/"
-                        + animal_name
-                        + "/"
-                        + session["date"]
-                        + "/[0-9]"
-                    )
-                ]
-                if session["experiment_number"] in img_expt_nums:
-                    pass
-                else:
-                    print("ERROR: Imaging session not found")
-                    continue
-            else:
-                print("ERROR: Imaging session not found")
-                continue
-
-        #
-        #
-        #     trial_files = sorted(glob.glob(imaging_dir + '/*.tif'))
-        #     if len(trial_files) != num_imaging_trials:
-        #         print(len(trial_files), num_imaging_trials)
-        #         print('SESSION_ERROR: Mismatch in number of tiff files')
-        #     file_sizes = []
-        #     for t, trial_file in enumerate(trial_files):
-        #         file_sizes.append(os.stat(trial_file).st_size)
-        #
-        #     file_sizes = np.array(file_sizes)
-        #     if len(trial_files) > 0:
-        #         mean_file_size = np.mean(file_sizes)
-        #         error_t = np.where(
-        #             np.abs((file_sizes - mean_file_size) / mean_file_size) > 0.01
-        #         )[0]
-        #         for et in error_t:
-        #             print(
-        #                 'TRIAL_ERROR: '
-        #                 + trial_files[et]
-        #                 + ' file size ('
-        #                 + str(file_sizes[et])
-        #                 + ' bytes) not close to mean ('
-        #                 + str(mean_file_size)
-        #                 + ' bytes)'
-        #             )
-        #
-        #     print()
-        # print()
-    print()
-
-    return
-
-
-def csv_with_behaviour(csv_path, behaviour_path, animals):
-    print("##################################################")
-    print("CSV WITH BEHAVIOUR")
-    print("##################################################")
-
-    for animal_name in animals:
-        csv_data = pd.read_csv(
-            csv_path + "/" + animal_name + ".csv",
-            dtype={
-                "upi": np.int64,
-                "date": str,
-                "experiment_number": str,
-                "num_behaviour_trials": np.int64,
-            },
-        )
-
-        print("--------------------------------------------------")
-        print(animal_name)
-        print("--------------------------------------------------")
-
-        bhvr_sess_paths = glob.glob(
-            behaviour_path + "/" + animal_name + "/" + animal_name + "*"
-        )
-        bhvr_sess_names = [x.split("/")[-1] for x in bhvr_sess_paths]
-        for index, session in csv_data.iterrows():
-
-            print("**************************************************")
-            print(animal_name + session["date"] + "/" + session["experiment_number"])
-            print("**************************************************")
-
-            if session["date"] in bhvr_dates:
-                bhvr_expt_nums = [
-                    x.split("/")[-1]
-                    for x in glob.glob(
-                        imaging_path
-                        + "/"
-                        + animal_name
-                        + "/"
-                        + session["date"]
-                        + "/[0-9]"
-                    )
-                ]
-                if session["experiment_number"] in bhvr_expt_nums:
-                    pass
-                else:
-                    print("ERROR: Imaging session not found")
-                    continue
-            else:
-                print("ERROR: Imaging session not found")
-                continue
-        #
-        #     print('**************************************************')
-        #     print(date + '/' + experiment_number)
-        #     print('**************************************************')
-        #
-        #     trial_files = sorted(glob.glob(imaging_dir + '/*.tif'))
-        #     if len(trial_files) != num_imaging_trials:
-        #         print(len(trial_files), num_imaging_trials)
-        #         print('SESSION_ERROR: Mismatch in number of tiff files')
-        #     file_sizes = []
-        #     for t, trial_file in enumerate(trial_files):
-        #         file_sizes.append(os.stat(trial_file).st_size)
-        #
-        #     file_sizes = np.array(file_sizes)
-        #     if len(trial_files) > 0:
-        #         mean_file_size = np.mean(file_sizes)
-        #         error_t = np.where(
-        #             np.abs((file_sizes - mean_file_size) / mean_file_size) > 0.01
-        #         )[0]
-        #         for et in error_t:
-        #             print(
-        #                 'TRIAL_ERROR: '
-        #                 + trial_files[et]
-        #                 + ' file size ('
-        #                 + str(file_sizes[et])
-        #                 + ' bytes) not close to mean ('
-        #                 + str(mean_file_size)
-        #                 + ' bytes)'
-        #             )
-        #
-        #     print()
-        # print()
-    print()
-
-    return
-
-
 def cross_check_with_csv(csv_path, imaging_path, behaviour_path, animals):
 
     for animal_name in animals:
         csv_data = pd.read_csv(
             csv_path + "/" + animal_name + ".csv",
             dtype={
-                "upi": np.int64,
-                "date": str,
-                "experiment_number": str,
-                "num_imaging_trials": np.int64,
+                'upi': np.int64,
+                'date': str,
+                'experiment_number': str,
+                'missing_imaging_trials': str,
+                'skip_imaging_trials': str,
+                'missing_behaviour_trials': str,
+                'skip_behaviour_trials': str,
             },
         )
-
         print("--------------------------------------------------")
         print(animal_name)
         print("--------------------------------------------------")
@@ -206,7 +38,10 @@ def cross_check_with_csv(csv_path, imaging_path, behaviour_path, animals):
         for index, session in csv_data.iterrows():
 
             print("**************************************************")
-            print(session["date"] + "/" + session["experiment_number"])
+            print(
+                f"{session['date']}/{session['experiment_number']}\t\
+                {session['behaviour_code']}_{session['behaviour_session_number']}"
+            )
             print("**************************************************")
 
             if session["num_imaging_trials"] > 0:
@@ -223,7 +58,52 @@ def cross_check_with_csv(csv_path, imaging_path, behaviour_path, animals):
                         )
                     ]
                     if session["experiment_number"] in img_expt_nums:
-                        pass
+                        trial_files = sorted(
+                            glob.glob(
+                                imaging_path
+                                + "/"
+                                + animal_name
+                                + "/"
+                                + session["date"]
+                                + "/"
+                                + session["experiment_number"]
+                                + "/*.tif*"
+                            )
+                        )
+                        if len(trial_files) != session["num_imaging_trials"]:
+                            print("ERROR: Mismatch in number of imaging tiff files")
+                        else:
+                            csv_error_trials = set()
+                            if pd.notna(session['skip_imaging_trials']):
+                                csv_error_trials.update([int(x) for x in session['skip_imaging_trials'].split(';')])
+                            if pd.notna(session['missing_imaging_trials']):
+                                csv_error_trials.update(int(x) for x in session['missing_imaging_trials'].split(';'))
+
+                            file_sizes = []
+                            for trial_file in trial_files:
+                                file_sizes.append(os.stat(trial_file).st_size)
+                            file_sizes = np.array(file_sizes)
+                            max_file_size = np.max(file_sizes)
+
+                            if session['behaviour_code'] == 'Hr7':
+                                small_file_size = np.percentile(file_sizes, 40)
+                                error_t = np.where(np.logical_and(
+                                    ((max_file_size - file_sizes) / max_file_size) > 0.01,
+                                    np.abs((small_file_size - file_sizes) / small_file_size) > 0.01
+                                ))[0]
+                            else:
+                                error_t = np.where(
+                                    ((max_file_size - file_sizes) / max_file_size) > 0.01
+                                )[0]
+                            for et in error_t:
+                                et_t_num = int(trial_files[et].split('-')[-3])
+                                if et_t_num not in csv_error_trials:
+                                    print(
+                                        'ERROR: '
+                                        + trial_files[et].split('/')[-1]
+                                        + ' file size not close to max file size'
+                                    )
+
                     else:
                         print("ERROR: Imaging session not found")
                         continue
@@ -231,15 +111,60 @@ def cross_check_with_csv(csv_path, imaging_path, behaviour_path, animals):
                     print("ERROR: Imaging session not found")
                     continue
 
-            bhvr_sess_name_1 = f"{animal_name}_{session['behaviour_code']}_{session['behaviour_session_number']}"
-            bhvr_sess_name_2 = f"{animal_name}_{session['behaviour_session_number']}_{session['behaviour_code']}"
+            bhvr_sess_name = f"{animal_name}_{session['behaviour_code']}_{session['behaviour_session_number']}"
 
             if session["num_behaviour_trials"] > 0:
-                if bhvr_sess_name_1 in bhvr_sess_names or bhvr_sess_name_2 in bhvr_sess_names :
-                    pass
+                if bhvr_sess_name in bhvr_sess_names:
+                    trial_files = sorted(
+                        glob.glob(
+                            behaviour_path
+                            + "/"
+                            + animal_name
+                            + "/"
+                            + bhvr_sess_name
+                            + "/"
+                            + "/*.tif*"
+                        )
+                    )
+                    if len(trial_files) != session["num_behaviour_trials"]:
+                        print("ERROR: Mismatch in number of behaviour tiff files")
+                    else:
+                        csv_error_trials = set()
+                        if pd.notna(session['skip_behaviour_trials']):
+                            csv_error_trials.update([int(x) for x in session['skip_behaviour_trials'].split(';')])
+                        if pd.notna(session['missing_behaviour_trials']):
+                            csv_error_trials.update(int(x) for x in session['missing_behaviour_trials'].split(';'))
+
+                        file_sizes = []
+                        for trial_file in trial_files:
+                            file_sizes.append(os.stat(trial_file).st_size)
+                        file_sizes = np.array(file_sizes)
+                        max_file_size = np.max(file_sizes)
+
+                        if session['behaviour_code'] == 'Hr7':
+                            small_file_size = np.percentile(file_sizes, 40)
+                            error_t = np.where(np.logical_and(
+                                ((max_file_size - file_sizes) / max_file_size) > 0.01,
+                                np.abs((small_file_size - file_sizes) / small_file_size) > 0.01
+                            ))[0]
+                        else:
+                            error_t = np.where(
+                                ((max_file_size - file_sizes) / max_file_size) > 0.01
+                            )[0]
+                        for et in error_t:
+                            et_t_num = int(trial_files[et].split('/')[-1].split('.')[-2])
+                            if et_t_num not in csv_error_trials:
+                                print(
+                                    'ERROR: '
+                                    + trial_files[et].split('/')[-1]
+                                    + ' file size not close to max file size'
+                                )
                 else:
-                    print(f"ERROR: Behaviour session {bhvr_sess_name_1} or {bhvr_sess_name_2} not found")
+                    print(
+                        f"ERROR: Behaviour session {bhvr_sess_name} not found"
+                    )
                     continue
+            print()
         print()
         print()
 
